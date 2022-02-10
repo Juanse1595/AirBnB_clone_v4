ff2a11dd6ae349eeb31ef271e39a913a91e85be6
Mateo <mateog91@gmail.com>
2022-02-10 09:51:48 -0500

    UPD: corrected javascript

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,14 @@
-$(function () {
+$(document).ready(function () {
+  lst = [];
   $('input[type="checkbox"]').click(function () {
-  alert('click on checkbox')
-})
-})
-/*$(function () {
-  let amenities = document.querySelector('input[type="checkbox"]');
-  lst = []
-  amenities.addEventListener('change', function () {
-    if (amenities.checked) {
-      lst.push($(this).attr('data-name'));
-      alert('push')
-      alert($(this).attr('data-name'))
+    if (this.checked) {
+      lst.push($(this).attr("data-name"));
     } else {
-      let toRemove = $(this).attr('data-name')
-      lst = lst.filter(function(item) {
-        return item !== toRemove
-    });
+      let toRemove = $(this).attr("data-name");
+      lst = lst.filter(function (item) {
+        return item !== toRemove;
+      });
     }
-    $('div.amenities h4').text(lst.join(', '))
-  })
-})*/
+    $("div.amenities h4").text(lst.join(", "));
+  });
+});
