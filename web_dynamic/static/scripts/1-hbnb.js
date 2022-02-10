d905b8096d4250db2f988969feebd9227743d201
Juanse1595 <3309@holberstonschool.com>
2022-02-09 22:21:25 -0500

    FIX fixing the 1-hbnb.js script, doing tests

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,22 @@
 $(function () {
+  $('input[type="checkbox"]').click(function () {
+  alert('click on checkbox')
+})
+})
+/*$(function () {
   let amenities = document.querySelector('input[type="checkbox"]');
   lst = []
-  amenities.addEventListener(change, function (e) {
+  amenities.addEventListener('change', function () {
     if (amenities.checked) {
-      lst.push(this.name);
+      lst.push($(this).attr('data-name'));
+      alert('push')
+      alert($(this).attr('data-name'))
     } else {
-      $(this)
+      let toRemove = $(this).attr('data-name')
+      lst = lst.filter(function(item) {
+        return item !== toRemove
+    });
     }
+    $('div.amenities h4').text(lst.join(', '))
   })
-})
+})*/
